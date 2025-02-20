fc6735a1616d2331587b8bf763ab820ee8bef5b6
rado-kainos <radoslaw.przybysz@kainos.com>
2025-02-20 17:09:46 +0000

    Rename registration_data -> data

--- ia_map_lib/datasets/datasets.py
+++ ia_map_lib/datasets/datasets.py
@@ -140,12 +140,12 @@ class SimpleDataSet(DataSet):
         return 'application/json'
 
     def registration_record(self, registration_fields: Mapping, headers: list[str | bytes | None] = None) -> Record:
-        registration_data = {
+        data = {
             'id': self.dataset_id,
             'title': self.title,
             'source_mime_type': self.source_mime_type
         }
-        record_data = {**registration_data, **registration_fields}
+        record_data = {**data, **registration_fields}
         return Record(headers, None, json.dumps(record_data), None)
 
     def update_record(self, headers: list[str | bytes | None] = None) -> Record:
